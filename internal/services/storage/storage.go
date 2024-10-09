58e61b17798528d5a63059bcd45ada558c58c294
Nikita <tyumin000@gmail.com>
2024-10-09 16:19:17 +0300

    feat: add business logic

--- internal/services/storage/storage.go
+++ internal/services/storage/storage.go
@@ -2,12 +2,21 @@ package storage
 
 import (
 	"context"
+	"errors"
+	"filemanager/internal/lib/random"
 	"filemanager/internal/models"
+	"filemanager/internal/storage"
+	"fmt"
 	"log/slog"
 )
 
-type FileSaver interface{}
-type FileProvider interface{}
+type FileSaver interface {
+	SaveFile(filename string, data []byte) (int, error)
+}
+type FileProvider interface {
+	File() ([]byte, error)
+	Files() ([]models.File, error)
+}
 
 type Storage struct {
 	log          *slog.Logger
@@ -27,15 +36,65 @@ func New(
 	}
 }
 
-func (s *Storage) SaveFile(fileData []byte) (bytes int, filename string) {
-	return 1, "test"
-}
-func (s *Storage) ListFiles(ctx context.Context) ([]models.File, error) {
-	files := []models.File{
-		{Name: "name", CreationDate: 1, UpdateDate: 1},
+func (s *Storage) SaveFile(ctx context.Context, fileData []byte) (int, string, error) {
+	const op = "storage.SaveFile"
+
+	filename := random.String(20)
+
+	log := s.log.With(slog.String("op", op), slog.String("filename", filename))
+
+	log.Info("trying to save file")
+
+	bytes, err := s.fileSaver.SaveFile(filename, fileData)
+	if err != nil {
+		log.Error("failed to save file", slog.Any("error", err))
+
+		return 0, "", fmt.Errorf("%s: %w", op, err)
 	}
+
+	log.Info("file saved successfully")
+
+	return bytes, filename, nil
+}
+
+func (s *Storage) ListFiles(ctx context.Context) ([]models.File, error) {
+	const op = "storage.ListFiles"
+
+	log := s.log.With(slog.String("op", op))
+
+	log.Info("retrieving files")
+
+	files, err := s.fileProvider.Files()
+	if err != nil {
+		log.Error("failed to retrieve files", slog.Any("error", err))
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("files retrieved successfully")
+
 	return files, nil
 }
+
 func (s *Storage) File(ctx context.Context, filename string) ([]byte, error) {
-	return make([]byte, 0), nil
+	const op = "storage.File"
+
+	log := s.log.With(slog.String("op", op), slog.String("filename", filename))
+
+	log.Info("trying to find file")
+
+	fileData, err := s.fileProvider.File()
+	if err != nil {
+		if errors.Is(err, storage.ErrFileNotFound) {
+			log.Warn("file is not found", slog.Any("error", err))
+
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+
+		log.Error("failed to retrieve file", slog.Any("error", err))
+
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return fileData, nil
 }
